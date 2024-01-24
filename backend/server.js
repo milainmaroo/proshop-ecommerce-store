7e377ffd0bb1ad5d92eb8a4450194c66b273e330
Mi Lainmar Oo <milainmar13@gmail.com>
2024-01-24 13:13:05 +1100

    Serving & Fetching Data

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import products from './data/products.js'
+const port = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
+
+// Products Route
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+// Single Product Route
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
+
+app.listen(port, () =>
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${port}`
+  )
+)
