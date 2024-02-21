ae1567280403eebe6d59ffdfdce2cd2305510bb8
Mi Lainmar Oo <milainmar13@gmail.com>
2024-02-21 12:01:20 +1100

    Create SearchBox and Search Products Routes

---
+++ frontend/src/components/SearchBox.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useParams, useNavigate } from 'react-router-dom'
+
+
+const SearchBox = () => {
+  const navigate = useNavigate()
+  const { keyword: urlKeyword } = useParams()
+  const [keyword, setKeyword] = useState(urlKeyword)
+
+  return (
+    <div>SearchBox</div>
+  )
+}
+
+export default SearchBox
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import { useGetProductsQuery } from '../slices/productsApiSlice'
@@ -8,9 +8,9 @@ import Message from '../components/Message'
 import Paginate from '../components/Paginate'
   
 const HomeScreen = () => {
-  const { pageNumber } = useParams()
+  const { pageNumber, keyword } = useParams()
 
-  const { data, isLoading, error } = useGetProductsQuery({pageNumber})
+  const { data, isLoading, error } = useGetProductsQuery({ keyword, pageNumber })
 
   return (
     <div>
@@ -28,7 +28,7 @@ const HomeScreen = () => {
               </Col>
             ))}
           </Row>
-          <Paginate pages={data.pages} page={data.page} />
+          <Paginate pages={data.pages} page={data.page} keyword={keyword ? keyword : ''} />
         </>
       )}
 
